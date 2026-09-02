122c2eb5640c61525618a394bb8feec382f8d5e6
Luís Gustavo <luisgustavocena@hotmail.com>
2026-09-02 16:19:23 -0300

    Add README and DATA

---
+++ notebook/analise.py
@@ -0,0 +1,37 @@
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+
+nomes_portugues = {
+    'show_id': 'id_do_show',
+    'type': 'tipo',
+    'title': 'titulo',
+    'director': 'diretor',
+    'cast': 'elenco',
+    'country': 'pais',
+    'date_added': 'data_de_adicao',
+    'release_year': 'ano_de_lancamento',
+    'rating': 'classificacao',
+    'duration': 'duracao',
+    'listed_in': 'listado_em',
+    'description': 'descricao',
+}
+
+netflix = pd.read_csv('../data/netflix_titles.csv')
+netflix.fillna('Desconhecido', inplace=True)
+netflix = netflix.rename(columns = nomes_portugues)
+
+#%%
+
+#Quantidade de Shows por País em um determinado ano de lançamento
+netflix['pais'] = netflix['pais'].str.split(',')
+netflix = netflix.explode('pais')
+netflix['pais'] = netflix['pais'].str.strip()
+netflix_ano = netflix[(netflix['ano_de_lancamento'] > 2000) & (netflix['ano_de_lancamento'] < 2020)]
+df_final_1 = netflix_ano.groupby(['ano_de_lancamento', 'pais']).size().reset_index( name = 'quantidade')
+
+
+
+# %%
+
+
