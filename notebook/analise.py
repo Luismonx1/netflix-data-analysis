4dcb07040afe546a6abf1b4947e1d5f6d311080f
Luís Gustavo <luisgustavocena@hotmail.com>
2026-09-13 16:45:59 -0300

    Conclusão pergunta 2 com scatter plot.

--- notebook/analise.py
+++ notebook/analise.py
@@ -36,7 +36,6 @@ df_paises = df_ano_pais.groupby('pais')['quantidade'].sum().reset_index(name='qu
 df_paises = df_paises[df_paises['pais'] != 'Desconhecido']
 
 top_10_paises = df_paises.sort_values(by='quantidade', ascending=False).head(10)
-top_10_paises.head(10)
 
 plt.barh(top_10_paises['pais'], top_10_paises['quantidade'])
 plt.title('Top 10 Países com Mais Shows na Netflix (2000-2020)')
@@ -60,14 +59,25 @@ df_data_adicao = df_data_adicao[df_data_adicao['data_de_adicao'] != 'Desconhecid
 df_data_adicao['pais'] = df_data_adicao['pais'].str.strip()
 df_data_adicao = df_data_adicao[df_data_adicao['pais'] != 'Desconhecido']
 
-
 df_data_adicao['data_de_adicao'] = pd.to_datetime(df_data_adicao['data_de_adicao'])
 
 df_data_ordenada = df_data_adicao.sort_values(by='data_de_adicao', ascending=True)
 df_data_ordenada = df_data_ordenada[df_data_ordenada['pais'] != '']
 
 
-df_mais_antigo = df_data_ordenada.groupby('pais').first()
-df_mais_recente = df_data_ordenada.groupby('pais').last()
+df_mais_antigo = df_data_ordenada.groupby('pais').first().reset_index()
+df_mais_recente = df_data_ordenada.groupby('pais').last().reset_index()
+
+top_5_paises = top_10_paises['pais'].head(5)
+
+df_mais_antigo_top5 = df_mais_antigo[df_mais_antigo['pais'].isin(top_5_paises)]
+df_mais_recente_top5 = df_mais_recente[df_mais_recente['pais'].isin(top_5_paises)]
+
+plt.scatter(x=df_mais_antigo_top5['data_de_adicao'], y=df_mais_antigo_top5['pais'], color='blue', label='Mais Antigo')
+plt.scatter(x=df_mais_recente_top5['data_de_adicao'], y=df_mais_recente_top5['pais'], color='orange', label='Mais Recente')
+plt.title('Shows Mais Antigos e Mais Recentes Adicionados na Netflix por País')
+plt.xlabel('Data de Adição')
+plt.ylabel('País')
+plt.legend()
 
 # %%
